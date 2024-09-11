e5d5f5af5ab5cc5f167f077c2e6a84390e2e505f
Aki Fukuda <akif.3839@keio.jp>
2024-09-11 10:01:55 +0900

    Initial Commit

---
+++ lemon wordcloud/lemon_wordcloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  lemon_wordcloudApp.swift
+//  lemon wordcloud
+//
+//  Created by 福田　陽 on 2024/09/11.
+//
+
+import SwiftUI
+
+@main
+struct lemon_wordcloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
